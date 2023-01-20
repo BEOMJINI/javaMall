32206501b2fcc9745324457d44a4e0810b483457
KIMBEOMJIN <kimbj0117@gmail.com>
2023-01-20 15:31:14 +0900

    .
    .

--- mallProject_kbj/src/s0_Mall/_Main.java
+++ mallProject_kbj/src/s0_Mall/_Main.java
@@ -14,9 +14,6 @@ public class _Main {
 		ItemController.getInstance().init();
 		MallController.getInstance().mallMenu();
 		System.out.println("dev에서 수정");
-		System.out.println("dev에서 수정");
-		System.out.println("dev에서 수정");
-		System.out.println("dev에서 수정");
-		System.out.println("dev에서 수정");
+		
 	}
 }
